80b9c9cfbfb7cff6b597bf7e597236b1bd079d80
dplusd <dplusd@gmail.com>
2016-03-28 23:46:03 +0300

    updated scripts

---
+++ source/ClassProducer/ClassProducer/scripts/Controller.js
@@ -0,0 +1,106 @@
+﻿var timer = null;
+var playState = null;
+function audioLoaded() {
+    audioHandler = document.getElementById("audioStream");
+
+    audioHandler.play()
+    audioHandler.playbackRate = 1;
+    setPlayState("play");
+
+}
+function startTimer() {
+    if (timer != null)
+        return;
+    timer = window.setInterval(syncText, 500);
+}
+var audioHandler;
+var selectedPar = null;
+function syncText() {
+    time = audioHandler.currentTime;
+    for (var i = 0 ; i < jsonParagraphs.length ; ++i) {
+        if (time >= jsonParagraphs[i].StartAudioPos && time < jsonParagraphs[i].EndAudioPos) {
+
+            if (jsonParagraphs[i].domElement.className != jsonParagraphs[i].selectedClass) {
+                jsonParagraphs[i].domElement.className = jsonParagraphs[i].selectedClass
+                scrollToPar(jsonParagraphs[i].domElement)
+                selectedPar = i;
+            }
+        }
+        else if (jsonParagraphs[i].domElement.className != jsonParagraphs[i].defaultClass) {
+            jsonParagraphs[i].domElement.className = jsonParagraphs[i].defaultClass;
+        }
+    }
+}
+var mode = "arabic"
+function toTaatik(text) {
+    var retVal = ""
+    for (i = 0 ; i < text.length ; ++i) {
+        if (taatikMap[text[i]] === undefined)
+            retVal += text[i]
+        else
+            retVal += taatikMap[text[i]]
+    }
+    return retVal;
+
+}
+function switchTatik() {
+    if (mode == "arabic") {
+        for (j = 0 ; j < jsonParagraphs.length ; ++j) {
+            Text = ""
+            par = jsonParagraphs[j]
+
+            for (ln = par._StartLine ; ln < par._EndLine ; ++ln) {
+                Text += toTaatik(lines[ln]) + "<br>"
+            }
+            par.domElement.innerHTML = Text;
+            document.getElementById("btnTaatik").src = "https://raw.githubusercontent.com/dplusd/LaBaignoireRoot/master/source/ClassProducer/ClassProducer/scripts/arab.png"
+        }
+        mode = "taatik"
+    }
+    else if (mode == "taatik") {
+        for (j = 0 ; j < jsonParagraphs.length ; ++j) {
+            Text = ""
+            for (ln = jsonParagraphs[j]._StartLine ; ln < jsonParagraphs[j]._EndLine ; ++ln)
+                Text += lines[ln] + "<br>"
+            jsonParagraphs[j].domElement.innerHTML = Text;
+            document.getElementById("btnTaatik").src = "https://raw.githubusercontent.com/dplusd/LaBaignoireRoot/master/source/ClassProducer/ClassProducer/scripts/taatik.png"
+        }
+        mode = "arabic"
+    }
+}
+function scrollToPar(par) {
+    topPos = par.offsetTop;
+    document.getElementById('wrapper').scrollTop = topPos - 200;
+}
+function setSpeed(change) {
+    document.getElementById("audioStream").playbackRate = document.getElementById("audioStream").playbackRate + change;
+}
+
+function goToPar(which) {
+    if (selectedPar == null)
+        return;
+    if (selectedPar + which < 0 || selectedPar + which >= jsonParagraphs.length)
+        return
+    document.getElementById("audioStream").currentTime = jsonParagraphs[selectedPar + which].StartAudioPos;
+    document.getElementById("audioStream").play()
+    setPlayState("play");    
+}
+
+function stop() {
+    audioHandler = document.getElementById("audioStream");
+
+    
+    if (playState == "play") {
+        audioHandler.pause()
+        setPlayState("pause");
+    }
+    else if (playState == "pause") {
+        audioHandler.play()
+        setPlayState("play");
+    }
+}
+
+function setPlayState(newPlayState) {
+    playState = newPlayState;
+    document.getElementById("btStopPause").src = "https://raw.githubusercontent.com/dplusd/LaBaignoireRoot/master/source/ClassProducer/ClassProducer/scripts/" + (playState == "play" ? "pause" : "play") + ".png"
+}
---
+++ source/ClassProducer/ClassProducer/scripts/JSonParser.js
@@ -0,0 +1,67 @@
+﻿var jsonParagraphs = null;
+var lines = null;
+function processStory(json) {
+    var story = JSON.parse(json);
+    lines = story.Lines;
+    var paragraphs = story.Paragraphs
+    mainDiv = document.getElementById("contentDiv");
+    for (i = 0 ; i < paragraphs.length ; ++i) {
+        newP = document.createElement("P")
+        var Text = "";
+        for (ln = paragraphs[i]._StartLine ; ln < paragraphs[i]._EndLine ; ++ln)
+            Text += lines[ln] + "<br>"
+        newP.innerHTML = Text;
+        if (i == 0) {
+
+            paragraphs[i].defaultClass = "header"
+            paragraphs[i].selectedClass = "headerSelected"
+        }
+        else {
+            paragraphs[i].defaultClass = "par"
+            paragraphs[i].selectedClass = "parSelected"
+        }
+
+        newP.className = paragraphs[i].defaultClass
+        paragraphs[i].domElement = newP;
+        mainDiv.appendChild(newP);
+    }
+
+    jsonParagraphs = paragraphs;
+}
+var taatikMap = {};
+function processTaatikMap(map) {
+    var charMap = JSON.parse(map);
+    for (i = 0 ; i < charMap.length ; ++i)
+        taatikMap[charMap[i].arabic] = charMap[i].heb;
+}
+var request = new XMLHttpRequest();
+
+request.open("GET", ScripJsonSource, true);
+request.onload = function (e) {
+    if (request.readyState === 4) {
+        if (request.status === 200) {
+            processStory(request.responseText);
+        } else {
+            console.error(request.statusText);
+        }
+    }
+};
+request.onerror = function (e) {
+    console.error(request.statusText);
+};
+request.send(null)
+var requestTaatik = new XMLHttpRequest();
+requestTaatik.open("GET", TaaticScriptSource, true);
+requestTaatik.onload = function (e) {
+    if (requestTaatik.readyState === 4) {
+        if (requestTaatik.status === 200) {
+            processTaatikMap(requestTaatik.responseText);
+        } else {
+            console.error(requestTaatik.statusText);
+        }
+    }
+};
+request.onerror = function (e) {
+    console.error(requestTaatik.statusText);
+};
+requestTaatik.send(null)
